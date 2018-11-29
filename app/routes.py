6ab09324038a31d872275a414bc7124061fa204a
Dustin Bischke <dustin@bischke.ca>
2018-11-29 15:43:41 +0000

    List user's ghost collection

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user
 from app import app, db
-from app.models import User
+from app.models import User, Ghost, GhostType
 from app.forms import LoginForm, RegistrationForm
 
 
@@ -14,7 +14,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('ghosts'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -22,7 +22,7 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        return redirect(url_for('ghosts'))
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
@@ -34,7 +34,7 @@ def logout():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('ghosts'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data)
@@ -44,3 +44,9 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/ghosts', methods=['GET', 'POST'])
+def ghosts():
+    user = User.query.filter_by(username=current_user.username).with_entities(User.id)
+    my_ghosts = Ghost.query.filter_by(user_id = user)
+    return render_template('ghosts.html', title='Ghosts', ghosts=my_ghosts)
